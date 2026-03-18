e53182eb97f4eb151dab1c7c7ceff3253e3deb27
sarthak12789 <mrtechy404@gmail.com>
2026-03-18 22:57:58 +0530

    made p2p signaling server

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-const express = require("express");
-const http = require("http");
-const { Server } = require("socket.io");
-const cors = require("cors");
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import cors from "cors";
 
 const app = express();
-app.use(cors());
+app.use(cors({
+ origin: "*", // later put your Vercel URL
+  methods: ["GET", "POST"],
+}));
 
 const server = http.createServer(app);
 
@@ -14,21 +17,40 @@ const io = new Server(server, {
     methods: ["GET", "POST"],
   },
 });
-
+app.get("/",(req,res)=>{
+  res.end("server is running");
+});
 io.on("connection", (socket) => {
   console.log("User connected:", socket.id);
 
-socket.on("send_message", (data) => {
-  socket.broadcast.emit("receive_message", data);
+socket.on("join_room", ({email, room}) => {
+ socket.to(room).emit("user_joined", room);
+ socket.join(room);
+ console.log(`${email} joined room: ${room}`);
 });
 
-
+socket.on("offersend",({offer,room})=>{
+  console.log("Offer received from", socket.id);
+ // console.log("Offer:", offer);
+  socket.to(room).emit("offerreceived", {offer,room});
+  console.log("Offer sent to room:", room);
+})
+socket.on("answer",({answer,room})=>{
+  console.log("Answer received from", socket.id);
+ // console.log("Answer:", answer);
+  socket.to(room).emit("answerreceived", answer);
+  console.log("Answer sent to room:", room);
+}
+)
+socket.on("ice", ({ candidate, room }) => {
+  socket.to(room).emit("ice", { candidate });
+});
   socket.on("disconnect", () => {
     console.log("User disconnected:", socket.id);
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 
 server.listen(PORT, () => {
   console.log("Server running on port", PORT);
